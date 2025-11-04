ee54d0734861cdd7b6150121ef8518278afb67a8
abigail <lau_rei_yan_abigail@s2023.ssts.edu.sg>
2025-11-04 15:24:43 +0800

    Collision without conservation of momentum ..

--- granularconvection.py
+++ granularconvection.py
@@ -1,3 +1,4 @@
+# rmb to change directory if needed
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
@@ -12,13 +13,13 @@ current_directory = os.getcwd()
 
 ### LISTS
 g = np.array([0.0, -9.8, 0.0])  # (gravitational field strength)
-s = np.array([[ # 0 to 0.2
-    [0.12, 0.1, 0], # x, y, z
+s = np.array([[ # (0 to 0.2) (3d array)
+    [0.12, 0.1, 0], # (x, y, z)
     [0.1, 0.2, 0],
     [0.13, 0.19, 0],
     [0, 0.16, 0]
-    ]])
-v = np.array([[ 
+    ]]) 
+v = np.array([[  # velocity of travel
     [0.2, 0.6, 0],
     [0.02, 0.3, 0],
     [-0.3, 0.01, 0],
@@ -39,11 +40,11 @@ def WRITE(file, data):
         writer.writerows(data)
         print(f"data written to '{file}'")
     
-def get_Vol(R):
+def get_Vol(R): # (scalar)
     vol = (4/3)*math.pi*(R**3)
     return vol
 
-def get_m(Vol, rho):
+def get_m(Vol, rho): # (scalar)
     return Vol*rho
 
 def get_W(m, g):
@@ -58,19 +59,19 @@ def get_Fdrag(Mu, Ri, Vi): # (air resistance using stokes formula)
     Fdrag = Ri_new*Vi # vi is. at one t step. ok.
     return Fdrag
 
-def get_allSij(Si, Sj): # 2d arrays
-    size = len(Si)
+def get_allSij(latest_s): # (3d arrays) 
+    size = len(latest_s)
     
-    Sij = np.array([[np.array([0.0]*3)]*size]*size)
+    allSij = np.array([[np.array([0.0]*3)]*size]*size)
     for row in range(size):
-        for col in range(row+1, size):
-            Sij[row][col] = Si[row][col] - Sj[row][col]
-            Sij[col][row] = -(Si[row][col] - Sj[row][col])
-    return Sij
+        for col in range(size):
+            allSij[row][col] = latest_s[row] - latest_s[col]
+            allSij[col][row] = -(latest_s[row] - latest_s[col])
+    return allSij
 
-def get_allHij(R, allSij):
-    size = len(R)
-    allHij = np.array([[0.0]*size]*size)
+def get_allHij(R, allSij): #(2d array) 
+    size = len(R) # number of particles
+    allHij = np.zeros((size, size))
     allSij_magnitudes = get_allSij_magnitudes(allSij)
     
     for row in range(size):
@@ -79,22 +80,22 @@ def get_allHij(R, allSij):
             allHij[col][row] = allHij[row][col]
     return allHij
 
-def get_magnitude(s):
+def get_magnitude(s): # (3d array)
     return math.sqrt(sum(i**2 for i in s))
 
-def get_unit_vector(s): # particle
+def get_unit_vector(s): # (particle)
     return s/get_magnitude(s)
 
-def get_allSij_magnitudes(allSij):
+def get_allSij_magnitudes(allSij): # (2d array)
     size = len(allSij)
     allSij_magnitudes = np.array([[0.0]*size]*size)
     for row in range(size):
         for col in range(size):
             allSij_magnitudes[row][col] = get_magnitude(allSij[row][col])
 
-    return allSij_magnitude
+    return allSij_magnitudes
 
-def get_allReff(R):
+def get_allReff(R): # (2d array)
     size = len(R)
     allReff = np.array([[0.0]*size]*size)
     for row in range(size):
@@ -106,8 +107,38 @@ def get_allReff(R):
 def get_Etilde(youngE, possionNu):
     return youngE/(1-possionNu**2)
 
+def get_allSijhat(allSij): # (3d array)
+    allSij_magnitudes = get_allSij_magnitudes(allSij)
+    allSijhat = allSij/allSij_magnitudes[:, :, np.newaxis]
+    # return np.nan_to_num(allSijhat)
+    return allSijhat
 
-def create_a(Fnet, m):
+def get_allfij(R, Etilde, allReff, allHij, allSijhat): # (3d array)
+    size = len(R)
+    allfij = np.full((size, size, 3), (2/3) * Etilde)
+    for row in range(size):
+        for col in range(size):
+            if row == col:
+                allfij[row][col] = 0
+            else:
+                allfij[row][col] = (allfij[row][col]*
+                                    (math.sqrt(allReff[row][col]))*
+                                    ((max(allHij[row][col], 0.0))**(3/2))*
+                                    allSijhat[row][col]
+                                    ) 
+                allfij[col][row] = allfij[row][col]
+    return allfij
+
+def get_allfi(R, allfij): # (2d array)
+    size = len(R)
+    allfi = np.array([[0.0]*size]*size)
+    for a, row in enumerate(allfij):
+        for b, col in enumerate(row):
+            for tobesum in range(3):
+                allfi[a][b] += col[tobesum]
+    return allfi
+
+def create_a(Fnet, m): 
     m = np.transpose([m])@np.array([[1,1,1]])
 
     return [Fnet/m]
@@ -208,5 +239,6 @@ out.release()
 print(f"video '{video_name}' created successfully")
 
 #==== write to csv ====#
+
 os.chdir(r"/Users/Abigail/Desktop/Sciences/ISS/ISS2.0/")
-WRITE("data.csv", [[f"particle{i+1}" for i in range(len(R))], *s.tolist()])
+WRITE("data.csv", [[f"particle{i+1}" for i in range(len(R))], *s.tolist()]) # stores each particle's coordinates for each timestep
